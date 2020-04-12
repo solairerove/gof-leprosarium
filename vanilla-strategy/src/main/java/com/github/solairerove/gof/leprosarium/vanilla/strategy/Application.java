9d03471ebd58c57f8427031792e1ae1a4671b210
Mikita Kryvitski <solairerove@gmail.com>
2020-04-12 16:43:10 +0300

    feature: add vanilla strategy stub maven module

---
+++ vanilla-strategy/src/main/java/com/github/solairerove/gof/leprosarium/vanilla/strategy/Application.java
@@ -0,0 +1,7 @@
+package com.github.solairerove.gof.leprosarium.vanilla.strategy;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
